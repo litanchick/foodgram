95471cdd47dc307bca11736ef6f4490a7b8ef832
Анна Берашевич <litanchick@yandex.ru>
2024-08-03 03:28:53 +0700

    delete frontend tests

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -7,8 +7,8 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from users.models import ListSubscriptions
+
 from foodgram.settings import ALLOWED_HOSTS
 
 from .filtres import NameFilter, RecipeFilter
